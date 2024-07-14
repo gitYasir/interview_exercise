3e65a9a3a54e226507c0c41a68482a604d7dab02
Yasir <yasir.ibrahim@hotmail.co.uk>
2024-07-14 14:04:14 +0100

    added code to fix failing test

--- src/message/message.data.ts
+++ src/message/message.data.ts
@@ -42,7 +42,6 @@ export class MessageData {
     return chatMessageToObject(message);
   }
 
-
   async getChatConversationMessages(
     data: GetMessageDto,
   ): Promise<PaginatedChatMessages> {
@@ -88,8 +87,19 @@ export class MessageData {
   }
 
   async delete(messageId: ObjectID): Promise<ChatMessage> {
-    // TODO allow a message to be marked as deleted
-    return new ChatMessage() // Minimum to pass ts checks -replace this
+    const filterBy = { _id: messageId };
+    const updateProperty = { deleted: true };
+    const deletedMessage = await this.chatMessageModel.findOneAndUpdate(
+      filterBy,
+      updateProperty,
+      {
+        new: true,
+        returnOriginal: false,
+      },
+    );
+    if (!deletedMessage)
+      throw new Error('The message to delete does not exist');
+    return chatMessageToObject(deletedMessage);
   }
 
   async resolve(messageId: ObjectID): Promise<ChatMessage> {
